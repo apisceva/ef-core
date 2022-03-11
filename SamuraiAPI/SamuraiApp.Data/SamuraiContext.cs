2cc24d78e05dd1e52e7495ced26e57e2b2ccb655
apisceva <aljona.gromova@gmail.com>
2022-03-11 06:00:23 -0500

    adding asp.net core project

---
+++ SamuraiAPI/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Domain;
+using System;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData",
+                options => options.MaxBatchSize(100))
+            //.LogTo(message => Debug.WriteLine(message))
+            .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name,
+                                              DbLoggerCategory.Database.Transaction.Name },
+                                LogLevel.Debug)
+            .EnableSensitiveDataLogging();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Samurai>()
+                .HasMany(s => s.Battles)
+                .WithMany(b => b.Samurais)
+                .UsingEntity<SamuraiBattle>
+                (bs => bs.HasOne<Battle>().WithMany(),
+                 bs => bs.HasOne<Samurai>().WithMany())
+                .Property(bs => bs.DateJoined)
+                .HasDefaultValueSql("getdate()");
+
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+            modelBuilder.Entity<SamuraiBattleStat>().HasNoKey().ToView("SamuraiBattleStats");
+
+        }
+    }
+}
